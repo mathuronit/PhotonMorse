21ea56d4fb97d78e650dd82993c4b01cc217b227
Srujan Mathur <srujanmathur@gmail.com>
2016-10-05 09:01:57 -0500

    HTML Prettify

--- morse.ino
+++ morse.ino
@@ -2,57 +2,45 @@
 int speakerPin = D3;
 int greenLED = A1;
 int redLED = A3;
+int redSwitch = D5;
+int greenSwitch = D4;
 int note = 550;
 int dotLen = 100;
 int dashLen = 3*dotLen;
 int spaceLen = 7*dotLen;
 String lastMessage = "";
 boolean searching = false;
-int redSwitch = D5;
-int greenSwitch = D4;
 
-const String morse[] = { ".-", "-...", "-.-.", "-..", 
-  ".","..-.","--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", 
-  ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", 
+const String morse[] = { ".-", "-...", "-.-.", "-..",
+  ".","..-.","--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+  ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
   "--.."};
 
 void setup() {
 pinMode(greenSwitch,INPUT_PULLUP); // But in reality i'm using blue :P
-pinMode(redSwitch,INPUT_PULLUP); // 
+pinMode(redSwitch,INPUT_PULLUP); //
 pinMode(speakerPin, OUTPUT);
 pinMode(redLED, OUTPUT);
 Serial.begin(9600);
-
-//signalMorse("...---...");
-//convertMorse("sos");
 Particle.function("morseMe",saveText);
 Particle.function("playSOS",playSOS);
 Particle.function("findMyPhoton",findPhoton);
 }
 
 void loop() {
-    if (searching){
+  if (searching){
       convertMorse("SOS");
-   //   searching = false;
   }
   if(digitalRead(redSwitch) == LOW) {
       Serial.println("Red Switch Pressed");
       searching = false;
-      
+
   }
   if(digitalRead(greenSwitch) == LOW) {
       Serial.println("Green Switch Pressed");
       convertMorse(lastMessage);
   }
-//   else if (digitalRead(redSwitch)==LOW)){
-//       searching = false;
-//   }
-//     if (digitalRead(greenSwitch)==LOW)){
-//       convertMorse(lastMessage);
-//   }
 }
-
-
 
 int playSOS(String msg){
     convertMorse(msg);
@@ -64,8 +52,6 @@ int findPhoton(String findme){
     return 1;
 }
 
-
-
 int saveText(String message){
   lastMessage = message;
   convertMorse(lastMessage);
@@ -74,25 +60,24 @@ int saveText(String message){
 
 void convertMorse(String message){
     int i = 0; //count variable
-  char C; //holds each letter 
-  while (message[i]) { 
+  char C; //holds each letter
+  while (message[i]) {
     C = toupper(message[i]);
-    if (C>=65 && C<=90) {  
+    if (C>=65 && C<=90) {
       signalMorse(morse[C-65]);
        Serial.print(morse[C-65]);
     }
     i++; // move to the next char
   }
   Serial.println();
- // delay(message.length()*dotLen);
 }
- 
+
 void signalMorse(String c) {
-  int i = 0; 
-  while (c[i]) { 
+  int i = 0;
+  while (c[i]) {
     digitalWrite(redLED,HIGH); // Turn On LED
     tone(speakerPin, note); // Buzz the Speaker
-    if (c[i] == '.') { 
+    if (c[i] == '.') {
        delay(dotLen);  // delay for dot secs
     } else if (c[i] == '-') {
        delay(dashLen); //delay for dash secs
@@ -100,7 +85,7 @@ void signalMorse(String c) {
     digitalWrite(redLED,LOW); // Turn off LED
     noTone(speakerPin); // Buzz off Speaker
     delay(dotLen); //
-    i++;    
+    i++;
   };
 
 }
